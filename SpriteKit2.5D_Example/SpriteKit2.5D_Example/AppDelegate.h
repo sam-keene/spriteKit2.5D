6b77a0caeba645a2b5580bce714fde3667f2f0f6
Sam  keene <samkeeneinteractivedesign@gmail.com>
2013-12-31 18:48:36 -0500

    setup repo

---
+++ SpriteKit2.5D_Example/SpriteKit2.5D_Example/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKit2.5D_Example
+//
+//  Created by Sam  keene on 31/12/13.
+//  Copyright (c) 2013 Sam  keene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SpriteKit2.5D_Example/SpriteKit2.5D_Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpriteKit2.5D_Example
+//
+//  Created by Sam  keene on 31/12/13.
+//  Copyright (c) 2013 Sam  keene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
